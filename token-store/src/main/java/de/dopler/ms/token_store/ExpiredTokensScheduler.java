e30c78f20b25c03d19be6b3d44d40b3479b837e4
Eduard Dopler <kontakt@eduard-dopler.de>
2020-05-21 16:34:45 +0200

    Add scheduler for deletion of expired tokens

---
+++ token-store/src/main/java/de/dopler/ms/token_store/ExpiredTokensScheduler.java
@@ -0,0 +1,42 @@
+package de.dopler.ms.token_store;
+
+import io.quarkus.scheduler.Scheduled;
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.time.Duration;
+import java.time.Instant;
+
+@ApplicationScoped
+public class ExpiredTokensScheduler {
+
+    private static final Logger LOG = Logger.getLogger("ExpiredTokensScheduler");
+
+    private static final long WARN_LOG_DURATION_THRESHOLD_SECONDS = 10;
+
+    private final TokenStoreService tokenStoreService;
+
+    @Inject
+    public ExpiredTokensScheduler(TokenStoreService tokenStoreService) {
+        this.tokenStoreService = tokenStoreService;
+    }
+
+    @Scheduled(every = "5m")
+    void deleteExpiredTokens() {
+        var startInstant = Instant.now();
+        var count = tokenStoreService.deleteExpired();
+        var duration = Duration.between(startInstant, Instant.now());
+        var tookMillis = duration.toMillis();
+
+        var message = String.format("Deleted %d expired tokens (took %d ms)", count, tookMillis);
+
+        if (duration.toSeconds() > WARN_LOG_DURATION_THRESHOLD_SECONDS) {
+            LOG.warn(message);
+        } else if (count > 0) {
+            LOG.info(message);
+        } else {
+            LOG.debug(message);
+        }
+    }
+}
